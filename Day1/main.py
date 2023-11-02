29d4598747b9ff863e091392ef04cdb884d6824a
PrashantK200105 <prashant.gurung303@gmail.com>
2023-11-02 13:48:43 +0000

    Day 1 Folder added

--- Day1/main.py
+++ Day1/main.py
@@ -1,3 +1,4 @@
+#write a code to generate a band name
 print("Welcome the band name generator")
 city = input("What is the name of the city you grew up?\n")
 pet = input("What is the name of your pet?\n")
