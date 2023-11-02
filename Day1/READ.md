4c044e84b26fae747271029096f8944cc5e78dd0
PrashantK200105 <prashant.gurung303@gmail.com>
2023-11-02 13:36:40 +0000

    Initial commit

---
+++ Day1/READ.md
@@ -0,0 +1,2 @@
+This file generate the name of band.
+https://replit.com/@prashantgurung6/100-Days-of-code#Day1/main.py
---
+++ Day1/main.py
@@ -0,0 +1,4 @@
+print("Welcome the band name generator")
+city = input("What is the name of the city you grew up?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
