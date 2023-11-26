3062046147ef7dd78e258f2615d834f0da98ef00
PrashantK200105 <prashant.gurung303@gmail.com>
2023-11-26 09:56:36 +0000

    Hangman completed

--- Day5/day5.py
+++ Day5/day5.py
@@ -1,32 +1,34 @@
 # This is the final project for day5
 #Password Generator Project
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-
-password =""
+password_list = []
 for char in range(1, nr_letters + 1):
-  string = random.choice(letters)
-  password = password + string
-
+  password_list += random.choice(letters)
 
 for sym in range(1, nr_symbols + 1):
-  sign = random.choice(symbols)
-  password = password + sign
+  password_list += random.choice(symbols)
 
 for num in range(1, nr_numbers + 1):
-  num = random.choice(numbers)
-  password = password + num
+  password_list += random.choice(numbers)
 
+random.shuffle(password_list)
 
-for final_password in range(0, len(password) + 1):
- final_password = random.choice(password)
- random_password = password + final_password
+random_password = ""
+for char in password_list:
+  random_password += char
 
-print(f"{random_password}")
+print(random_password)
